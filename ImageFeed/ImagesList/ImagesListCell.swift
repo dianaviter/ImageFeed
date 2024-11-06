9ceb443aef845d18a6f559b266810bd5282e65b6
Diana Viter <diviterok@gmail.com>
2024-11-06 18:11:50 +0300

    Выполнены условия задачи спринта 8

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Diana Viter on 05.11.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
