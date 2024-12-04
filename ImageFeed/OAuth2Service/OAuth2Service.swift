9d0f0b741ea44bff8e71fc94f541bd56c54f01cd
Diana Viter <diviterok@gmail.com>
2024-12-04 14:49:47 +0300

    доп изменения по комментам ревьюера

--- ImageFeed/OAuth2Service/OAuth2Service.swift
+++ ImageFeed/OAuth2Service/OAuth2Service.swift
@@ -21,11 +21,36 @@ final class OAuth2Service {
     private let tokenStorage = OAuth2TokenStorage()
     private let networkClient = NetworkClient()
     private let baseApiUrl = "https://unsplash.com"
-
+    
     static let shared = OAuth2Service()
-
     private init() {}
-
+    
+    func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let request = makeOAuthTokenRequest(code: code) else {
+            print("Failed to create URLRequest")
+            completion(.failure(OAuthError.urlEncodingError))
+            return
+        }
+        
+        networkClient.data(request: request) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let response = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
+                    self.tokenStorage.token = response.access_token
+                    completion(.success(response.access_token))
+                } catch {
+                    print("Decoding error: \(error)")
+                    completion(.failure(error))
+                }
+                
+            case .failure(let error):
+                print("Request error: \(error)")
+                completion(.failure(error))
+            }
+        }
+    }
+    
     private func makeOAuthTokenRequest(code: String) -> URLRequest? {
         guard let url = URL(string: "oauth/token", relativeTo: URL(string: baseApiUrl)) else {
             print("Failed to create URL with base: \(baseApiUrl)")
@@ -57,35 +82,7 @@ final class OAuth2Service {
         }
         
         urlRequest.httpBody = parameterArray.joined(separator: "&").data(using: .utf8)
-
-        return urlRequest
-    }
-    
-    func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
-        guard let request = makeOAuthTokenRequest(code: code) else {
-            print("Failed to create URLRequest")
-            completion(.failure(OAuthError.urlEncodingError))
-            return
-        }
         
-        networkClient.data(request: request) { result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let data):
-                    do {
-                        let response = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
-                        self.tokenStorage.token = response.access_token
-                        completion(.success(response.access_token))
-                    } catch {
-                        print("Decoding error: \(error)")
-                        completion(.failure(error))
-                    }
-                    
-                case .failure(let error):
-                    print("Request error: \(error)")
-                    completion(.failure(error))
-                }
-            }
-        }
+        return urlRequest
     }
 }
